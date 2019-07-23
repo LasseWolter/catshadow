0163f73b2d4b22e6ea283fa1c7bfb0e3d759c326
Lasse Wolter <wolter.lasse@gmail.com>
2019-07-23 12:25:40 +0100

    Using Poisson Proccess for sending drop traffic
    Uses the existing parameter lambdaP and allows for drop
    traffic to be sent
    -> Thus, if no real messages are added to the queue, a drop message is
    being sent every 1/lambdaP ms

--- experiment/main.go
+++ experiment/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/katzenpost/client/config"
 	"github.com/katzenpost/core/crypto/ecdh"
 	"github.com/katzenpost/core/crypto/rand"
-	"math"
 	"os"
 	"time"
 )
@@ -85,46 +84,20 @@ func main() {
 	fmt.Println("state worker started")
 	sender.Start()
 	fmt.Println("catshadow worker started")
-	messageNum := sendCfg.Experiment.MessageNum
-	blockSize := sendCfg.Experiment.BlockSize
-	interval := sendCfg.Experiment.Interval
 	expDuration := time.Duration(sendCfg.Experiment.Duration) * time.Minute
 	// Display Header of Experiment
 	printFigure("Mixnet", "epic")
 	printFigure("Experiment", "epic")
-	// If an experiment duration is given use this instead of sending `messageNum` messages
-	if expDuration > time.Duration(0) {
-		printFigure("by Duration", "small")
-		fmt.Printf("\nFound Duration in Config file. Ignoring MessageNum and will keep sending messages until "+
-			"experiment is over. Experiment will last: %v\n\n", expDuration)
-		running := true
-		// Boolean `running` will be set to false after expDuration and will cause the for loop below to stop
-		time.AfterFunc(expDuration, func() {
-			running = false
-		})
-		// Worker loop which keeps sending messages until the end of the experiment
-		for running == true {
-			for b := 0; b < blockSize; b++ {
-				sender.DoSendDropMsg()
-			}
-			fmt.Printf("Sent message block of %v message(s)\n", blockSize)
-			time.Sleep(time.Duration(interval) * time.Millisecond)
-		}
-	} else { // No experiment duratoin was given. The experiment will last unitl messageNum messages have been sent
-		printFigure("by Message Number", "small")
-		fmt.Printf("\nAbout to send %v messages in blocks of %v - time between message blocks: %vms\n", messageNum, blockSize, interval)
-		blockNum := math.Floor(float64(messageNum) / float64(blockSize))
-		for i := 0; i < int(blockNum); i++ {
-			for b := 0; b < blockSize; b++ {
-				sender.DoSendDropMsg()
-			}
-			fmt.Printf("Sent message block of %v message(s)\n", blockSize)
-			time.Sleep(time.Duration(interval) * time.Millisecond)
-		}
-	}
+
+	// Wait for expDuration - the sending happens automatically when lambdaP triggers
+	fmt.Printf("\nThe experiment will run for %v\n", expDuration)
+	fmt.Println("Messages are sent according to a Poisson Process")
+	time.Sleep(expDuration)
 	sender.Shutdown()
+
 }
 
+// Prints string in given font as Ascii-Art
 func printFigure(str string, font string) {
 	fig := figure.NewFigure(str, font, true)
 	fig.Print()
